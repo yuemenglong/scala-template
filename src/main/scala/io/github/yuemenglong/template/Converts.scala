3a8a364b7b18e43aa0636b88592ee69b3062346f
yuemenglong <yuemenglong@126.com>
2017-12-14 16:33:31 +0800

    fix: 子节点为null的情况

--- src/main/scala/io/github/yuemenglong/template/Converts.scala
+++ src/main/scala/io/github/yuemenglong/template/Converts.scala
@@ -4,7 +4,12 @@ package io.github.yuemenglong.template
   * Created by <yuemenglong@126.com> on 2017/11/6.
   */
 object Converts {
-  implicit def convertToRenderable[T](t: T): Renderable = { () => t.toString }
+  implicit def convertToRenderable[T](t: T): Renderable = { () =>
+    t match {
+      case null => ""
+      case _ => t.toString
+    }
+  }
 
   implicit def arrayToRenderable(arr: Array[Renderable]): Renderable = { () => arr.map(_.renderToHtml()).mkString("") }
 }
